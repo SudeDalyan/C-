a7701b5e936085715e936dc4e127b42ba5f9f877
Sude <64406311+SudeDalyan@users.noreply.github.com>
2020-05-01 15:50:30 +0300

    Add files via upload

---
+++ Kayıt Defteri/Kayıt Defteri/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kayıt_Defteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label8.Text = textBox1.Text;
+            label9.Text = textBox2.Text;
+            label10.Text = comboBox2.Text;
+            label11.Text = comboBox1.Text;
+            label12.Text = textBox5.Text;
+            label13.Text = textBox6.Text;
+            label14.Text = textBox7.Text;
+
+            
+            }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
