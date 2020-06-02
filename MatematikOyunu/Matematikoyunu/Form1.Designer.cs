9e5de1359da619bb7af1c5952c92e52609e321a4
Sude <64406311+SudeDalyan@users.noreply.github.com>
2020-06-02 23:28:13 +0300

    Add files via upload

---
+++ MatematikOyunu/Matematikoyunu/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Matematikoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matematik Oyunu";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(187, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "+";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(269, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "1";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(374, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "=";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(458, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 50);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "2";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.button1.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(111, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 75);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.button2.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(269, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 75);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "0";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(129)))));
+            this.button3.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(421, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 75);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Olive;
+            this.button4.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(166, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "BAŞLA";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Olive;
+            this.button5.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(380, 308);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 30);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "BİTİR";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(469, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Puan= ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(575, 27);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 23);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "0";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(157)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(634, 361);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ MatematikOyunu/Matematikoyunu/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matematikoyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double sy1, sy2, sy3;
+        int islem, s4, s5, s6;
+        int puan = 0;
+
+        private void basla()
+        {
+            Random rnd = new Random();
+            sy1 = rnd.Next(0, 30);
+            sy2 = rnd.Next(0, 30);
+            islem = rnd.Next(0, 4);
+            s4 = rnd.Next(0, 3);
+            s5 = rnd.Next(Convert.ToInt32(sy3 - 1), Convert.ToInt32(sy3 + 1));
+            s6 = rnd.Next(Convert.ToInt32(sy3 - 1), Convert.ToInt32(sy3 + 1));
+
+
+            label2.Text = sy1.ToString();
+            label4.Text = sy2.ToString();
+
+            if(islem==0)
+            {
+                label3.Text="+";
+                sy3 = sy1 + sy2;
+                label6.Text = sy3.ToString();               
+
+            }
+            if(islem==1)
+            {
+                label3.Text="-";
+                sy3 = sy1 - sy2;
+                label6.Text = sy3.ToString();
+                
+            }
+            if(islem==2)
+            {
+                label3.Text="/";
+                sy3 = sy1 / sy2;
+                label6.Text = sy3.ToString();
+              
+            }
+            if(islem==3)
+            {
+                label3.Text = "x";
+                sy3 = sy1 * sy2;
+                label6.Text = sy3.ToString();               
+            }
+        
+
+            if(s4==0)
+            {
+                button1.Text = sy3.ToString();
+                button2.Text = s5.ToString();
+                button3.Text = s6.ToString();
+
+
+            }
+            if(s4==1)
+            {
+                button2.Text = sy3.ToString();
+                button3.Text = s5.ToString();
+                button1.Text = s6.ToString();
+            }
+            if(s4==2)
+            {
+                button3.Text = sy3.ToString();
+                button1.Text = s5.ToString();
+                button2.Text = s6.ToString();
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            basla();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text==sy3.ToString())
+            {
+                puan = puan + 10;
+                MessageBox.Show("Kazandınız" + sy3.ToString());
+                label8.Text = puan.ToString();
+                
+            }
+            else
+            {
+                puan = puan - 10;
+                MessageBox.Show("Kaybettiniz" + sy3.ToString());
+                label8.Text = puan.ToString();
+            }
+            basla();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == sy3.ToString())
+            {
+                puan = puan + 10;
+                MessageBox.Show("Kazandınız" + sy3.ToString());
+                label8.Text = puan.ToString();
+
+            }
+            else
+            {
+                puan = puan - 10;
+                MessageBox.Show("Kaybettiniz" + sy3.ToString());
+                label8.Text = puan.ToString();
+            }
+            basla();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == sy3.ToString())
+            {
+                puan = puan + 10;
+                MessageBox.Show("Kazandınız" + sy3.ToString());
+                label8.Text = puan.ToString();
+
+            }
+            else
+            {
+                puan = puan - 10;
+                MessageBox.Show("Kaybettiniz" + sy3.ToString());
+                label8.Text = puan.ToString();
+            }
+            basla();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tebrikler Puanınız :" + puan.ToString());
+
+        }
+
+
+    }
+}
